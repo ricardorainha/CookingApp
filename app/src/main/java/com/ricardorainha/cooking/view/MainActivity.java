f64bde0edd40a834fa1e6e0b23f17b8490c34bf1
Ricardo Rainha <ricardofrainha@gmail.com>
2019-02-04 01:48:32 -0200

    Moving MainActivity to view package
    Signed-off-by: Ricardo Rainha <ricardofrainha@gmail.com>

--- app/src/main/java/com/ricardorainha/cooking/view/MainActivity.java
+++ app/src/main/java/com/ricardorainha/cooking/view/MainActivity.java
@@ -1,14 +1,25 @@
-package com.ricardorainha.cooking;
+package com.ricardorainha.cooking.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.ricardorainha.cooking.R;
+import com.ricardorainha.cooking.model.Recipe;
+import com.ricardorainha.cooking.model.Repository;
+
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+
 public class MainActivity extends AppCompatActivity {
+
+    Repository repo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
