6daa68faa6e4f66d4d898773d6565e5a85a0ce78
Ricardo Rainha <ricardofrainha@gmail.com>
2019-02-10 15:53:17 -0200

    Using GridLayout when device is in landscape orientation
    Signed-off-by: Ricardo Rainha <ricardofrainha@gmail.com>

--- app/src/main/java/com/ricardorainha/cooking/view/RecipesListFragment.java
+++ app/src/main/java/com/ricardorainha/cooking/view/RecipesListFragment.java
@@ -1,6 +1,7 @@
 package com.ricardorainha.cooking.view;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +13,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import butterknife.BindView;
@@ -48,7 +50,12 @@ public class RecipesListFragment extends Fragment {
     }
 
     private void setupViews() {
-        rvRecipes.setLayoutManager(new LinearLayoutManager(getContext()));
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            rvRecipes.setLayoutManager(new LinearLayoutManager(getContext()));
+        }
+        else {
+            rvRecipes.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        }
         rvRecipes.setHasFixedSize(true);
     }
 
